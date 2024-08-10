81967abdfbbd5b9022fea00291f8b702c2aee429
sai subham ray <saisubhamray@sais-MacBook-Air.local>
2024-08-11 00:33:17 +0530

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+import bookRoute from "./route/book.route.js";
+import userRoute from "./route/user.route.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "Frontend", "build")));
+  res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
+});
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const URI = process.env.MongoDBURI;
+
+// connect to mongoDB
+try {
+  mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("Connected to mongoDB");
+} catch (error) {
+  console.log("Error: ", error);
+}
+
+// defining routes
+app.use("/book", bookRoute);
+app.use("/user", userRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
